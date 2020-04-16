f268c05a1bd1d551fc8e138514d9ba159e4771f4
Mihai Atofanei <mihai.atofanei@gmail.com>
2020-04-17 08:20:28 +1000

    added redux and implemented userReducer and userAction to header;updated app to redirect to home if user is signed in; added cartIconand cartdropdown comp to our aplication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-
+import {Switch, Route, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -9,36 +9,28 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-sign-up.component/sign-in-sign-up.component'
 import {auth , createUserProfileDocument} from './firebase/firebase.utils';
- 
+import { setCurrentUser } from './redux/user/user.actions';
+
 
 class App extends React.Component {
-
-    constructor() {
-      super();
-
-      this.state = {
-        currentUser: null
-      }
-    }
-
     unsubscribeFromAuth = null
 
     componentDidMount() {
+      const { setCurrentUser } = this.props;
+
       this.unsubscribeFomAuth = auth.onAuthStateChanged(async userAuth => {
          if (userAuth) {
            const userRef = await createUserProfileDocument(userAuth);
 
            userRef.onSnapshot(snapShot => {
-             this.setState({
-               currentUser:{
+             setCurrentUser({
                  id: snapShot.id,
                  ...snapShot.data()
-               }
-             });
+               });
            });
          }
         
-        this.setState({currentUser: userAuth});
+        setCurrentUser({userAuth});
       });
     }
 
@@ -49,11 +41,19 @@ class App extends React.Component {
     render () {
       return (
         <div >
-          <Header currentUser={this.state.currentUser}/>
+          <Header />
           <Switch>
             <Route exact path='/' component = {HomePage}/>
             <Route exact path='/shop' component = {ShopPage}/>
-            <Route exact path='/signin' component = {SignInAndSignUpPage}/>
+            <Route exact path='/signin'  
+              render={() => 
+                this.props.currentUser ? (
+                  <Redirect to='/'/>
+                  ) : (<
+                    SignInAndSignUpPage/>
+                    )
+                  }
+                />
           </Switch>
         </div>
       );
@@ -61,4 +61,14 @@ class App extends React.Component {
    
   }
 
-export default App;
+  const mapStateToProps = ({ user }) => ({
+    curentuser: user.CurrentUser
+  })
+
+  const mapDispatchToProps = dispatch => ({
+     setCurrentUser: user => dispatch (setCurrentUser(user)) 
+  });
+
+export default connect(
+  mapStateToProps,
+   mapDispatchToProps ) (App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';
+
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { auth } from '../../firebase/firebase.utils';
 
@@ -7,11 +11,13 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden}) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
     </Link>
+
+
     <div className='options'>
       <Link className='option' to='/shop'>
         SHOP
@@ -28,8 +34,16 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon/>
     </div>
+    { hidden ? null : <CartDropdown/>}
+    
   </div>
 );
 
-export default Header;
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({ 
+    currentUser,
+    hidden
+});
+
+export default connect(mapStateToProps ) (Header);
