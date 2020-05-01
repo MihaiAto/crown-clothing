b89fcb4e46bb30a5b37904a9a17eab657da2cb7c
Mihai Atofanei <mihai.atofanei@gmail.com>
2020-05-01 17:51:32 +1000

    moved the directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,17 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js'
+import { Route } from 'react-router-dom';
 
-import CollectionPreview from '../../components/collection-preview.component/collection-preview.component';
 
-class ShopPage extends React.Component {
-    constructor (props) {
-        super(props);
+import CollectionPage from '../collection/collection.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+const ShopPage = ({ match }) => (
+     <div className= 'shop-page' >
+            <Route exact path = {`${match.path}`} component = { CollectionsOverview}/>
+            <Route path= { `${match.path}/:collectionId`} component = {CollectionPage}/>
+        </div>
+    ); 
 
-    render () {
-        const {collections} = this.state;
-        return( <div ClassName= 'shop-page' >
-            {
-                collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))
-            }
-        </div>);   
-    }
-           
-}
+
 
 export default ShopPage;
---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,28 @@
+import {createSelector} from 'reselect';
+import {memoize} from 'lodash';
+
+
+const selectShop = state => state.shop;
+
+export const selectCollections = createSelector (
+    [selectShop],
+    shop => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys ( collections ).map(key => collections[key])
+);
+
+//export const selectCollection = collectionUrlParam => 
+//createSelector(
+ //   [selectCollections],
+  //  collections => collections[collectionUrlParam]
+//);
+
+export const selectCollection = memoize((collectionUrlParam) => (
+    createSelector([selectCollections], (collections) => {
+      console.log(collections);
+      return collections[collectionUrlParam];
+    })
+  ));
