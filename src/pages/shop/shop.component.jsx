82e53b1bcd24dba0b369d6d38335384c3f5f3e55
Mihai Atofanei <mihai.atofanei@gmail.com>
2020-06-07 10:09:06 +1000

    adding redux-thunk for asyncronous event handleling and implemented the container pattern concept intoour shop selectors

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,59 +2,47 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import CollectionPage from '../collection/collection.component';
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
-
-import WithSpinner from '../../components/with-spinner/with-spinner.component'
-
-import { firestore,
-         convertCollectionsSnapshotToMap 
-        } from '../../firebase/firebase.utils';
-
-import { updateCollections } from '../../redux/shop/shop.actions';
+import CollectionPageContainer from '../collection/collection.container';
+import CollectionsOverviewContainer from '../../components/collections-overview/collection-overview.container'
 
 
-const CollectionsOverviewWithSpinner = WithSpinner (CollectionsOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+
+
 
 
 class ShopPage extends React.Component {
-    state = {
-        loading: true
-    };
-
-    unsubscribeFromSnapshot = null;
 
     componentDidMount() {
-        const  { updateCollections } = this.props
-        const collectionRef = firestore.collection('collections');
+        const { fetchCollectionsStartAsync } = this.props;
 
-        this.unsubscribeFromSnapshot =  collectionRef.onSnapshot(async snapshot => {
-           const collectionMap =  convertCollectionsSnapshotToMap(snapshot);
-           updateCollections(collectionMap);
-           this.setState({ loading: false });
-        });
+        fetchCollectionsStartAsync();
     }
 
     render () {
-        const { match } = this.props;
-        const {loading } = this.state; 
+        const { match, } = this.props;
+         
         return (
             <div className= 'shop-page' >
-                   <Route exact path = {`${match.path}`} render = {(props) => <CollectionsOverviewWithSpinner isLoading= {loading} {...props}/>}/>
-                   <Route path= { `${match.path}/:collectionId`}
-                    render = { props => (
-                        <CollectionPageWithSpinner isLoading= {loading} {...props}/>
-                        )}
+                   <Route
+                     exact 
+                     path = {`${match.path}`} 
+                     component = {CollectionsOverviewContainer}   
                     />
+                  
+                     <Route 
+                        path= { `${match.path}/:collectionId`}
+                        component = {CollectionPageContainer}
+                         />
                </div>
            ); 
     }
 } 
 
+
 const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap => 
-    dispatch (updateCollections(collectionsMap))
-})
+   fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+});
 
 export default connect ( null, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -24,3 +24,13 @@ export const selectCollection = collectionUrlParam =>
     createSelector([selectCollections], 
       collections =>( collections ? collections[collectionUrlParam] : null)
   );
+
+export const selectIsCollectionFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector (
+    [selectShop],
+    shop => !!shop.selections
+);
